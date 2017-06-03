916114f5accbcf6d0116f96e09509459f1652b68
iHealthLabs <jingzhiwei@ihealthlabs.com.cn>
2017-06-03 15:04:48 +0800

    New project with CoreData

---
+++ VSMDemo/VSM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VSM
+//
+//  Created by jing on 03/06/2017.
+//  Copyright © 2017 jing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("App launch...")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
